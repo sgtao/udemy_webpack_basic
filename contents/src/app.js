06ae254e091381aa6d003b135abee99592060d01
Shogo Ogami <sg.tao.so@gmail.com>
2023-04-04 08:35:59 +0900

    03_Webpackの設定ファイルを編集してみよう

---
+++ contents/src/app.js
@@ -0,0 +1,4 @@
+// import sub from './sub' // sub.js をインポートする(.jsは省略可能)
+import './sub' // sub.js をインポート(更に省略するケース)
+
+console.log('this is a main js')
---
+++ contents/webpack.config.js
@@ -0,0 +1,20 @@
+// CommonJs style
+// - module.exports = {...} にルールを記述する
+//   * 似たようなものにES6から、import/exportというのがある
+// 実行方法は：
+//   yarn run webpack
+// 
+const path = require('path'); // output で相対パスにするために読込
+//
+module.exports = {
+  // プロパティを定義する
+  mode: 'development',
+  // devtool: 'none', // これは使えないようだ
+  // devtool: 'inline-source-map', // distが暗号化される
+  // entry: ['./src/app.js', './src/sub.js']
+  entry: {app: './src/app.js'},
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].bundle.js'
+  }
+}
